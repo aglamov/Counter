6eb1f097cc0ceb4fbb319c3d199182d504487aca
aglamov <aglamov@yandex.ru>
2023-02-28 20:59:33 +0300

    Реализован новый механизм: кнопка и лейбл, изменение лейбла при нажатии на кнопку

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var countClick = 0
+    @IBOutlet weak var buttonController: UIButton!
+    @IBOutlet weak var labelController: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonAction(_ sender: Any) {
+        countClick += 1
+        labelController.text = "Значение счетчика: \(countClick)"
+    }
+    
 }
 
